1a864763af8bc33ca5a13d9cefc640915fe51e86
BetoHerbas <betitoherbas@gmail.com>
2025-05-27 23:17:16 -0400

    change backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://backendpeluditosya-d4h5fveubrgbhea9.canadacentral-01.azurewebsites.net/api' 
+    apiBaseUrl: 'backendpeluditosya-d4h5fveubrgbhea9.brazilsouth-01.azurewebsites.net/api' 
   };
   
